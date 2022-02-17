fa5039d5b664b10c51b8a4d8f6673e86a7914850
Babul Akter <babulakterfsd@gmail.com>
2022-02-17 09:34:07 +0600

    invironment setup done

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+const myName = 'false';
+const executor = (str) => {
+    return Boolean(str.toLowerCase());
+};
+console.log(executor(myName));
---
+++ index.ts
@@ -0,0 +1,7 @@
+const myName = 'false';
+
+const executor = (str:string) => {
+    return Boolean(str.toLowerCase());
+}
+
+console.log(executor(myName));
